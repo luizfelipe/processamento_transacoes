4ada058baca298cd15462caea7773cca355461c9
egon.ssena <egon.ssena@NTLD267802.GOV>
2018-09-23 02:36:26 -0300

    step de log

--- code_surfers-batch/src/main/java/com/codesurfers/xpto/Batch.java
+++ code_surfers-batch/src/main/java/com/codesurfers/xpto/Batch.java
@@ -1,7 +1,6 @@
 package com.codesurfers.xpto;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -26,14 +25,15 @@ import com.codesurfers.xpto.model.TransacaoFinanceira;
 import com.codesurfers.xpto.steps.Step1;
 import com.codesurfers.xpto.steps.Step2;
 import com.codesurfers.xpto.steps.Step3;
+import com.codesurfers.xpto.steps.Step4;
 
 @Configuration
 @EnableBatchProcessing
 public class Batch {
 
 	@Bean
-	public Job job(JobBuilderFactory jobs, StepBuilderFactory steps, Step1 step1, Step2 step2, Step3 step3,
-			EntityManagerFactory entityManagerFactory) throws MalformedURLException {
+	public Job job(JobBuilderFactory jobs, StepBuilderFactory steps, Step1 step1, Step2 step2, Step3 step3, Step4 step4,
+			EntityManagerFactory entityManagerFactory) throws Exception {
 
 		Step s1 = steps.get("baixar_descompactar").tasklet(step1.baixarEDescompactarArquivo()).build();
 
@@ -50,9 +50,12 @@ public class Batch {
 
 		Step s3 = steps.get("aplicar_regras").tasklet(step3.aplicarRegras()).build();
 
+		Step s4 = steps.get("gerar_log").<TransacaoFinanceira, TransacaoFinanceira>chunk(1000)
+				.reader(step4.reader(entityManagerFactory, null)).writer(step4.writer()).build();
+
 		return jobs.get("job").incrementer(new RunIdIncrementer())
 				.validator(new DefaultJobParametersValidator(new String[] { "ano" }, new String[] {})).start(s1)
-				.next(s2).next(s3).build();
+				.next(s2).next(s3).next(s4).build();
 	}
 
 	@Bean
