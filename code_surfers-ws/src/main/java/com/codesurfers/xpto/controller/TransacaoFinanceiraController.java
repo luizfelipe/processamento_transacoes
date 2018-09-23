3d640b399f1a92751fbf24c8191efead7a40d655
Luiz Felipe de Sousa Miranda - DATAPREVRN <luizfelipe.miranda@NTLD277667.GOV>
2018-09-23 07:45:51 -0300

    novo endpoint para buscar dados em outro formato

--- code_surfers-ws/src/main/java/com/codesurfers/xpto/controller/TransacaoFinanceiraController.java
+++ code_surfers-ws/src/main/java/com/codesurfers/xpto/controller/TransacaoFinanceiraController.java
@@ -1,6 +1,10 @@
 package com.codesurfers.xpto.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codesurfers.xpto.dto.TransacoesFinanceirasDatatableDTO;
 import com.codesurfers.xpto.model.TransacaoFinanceira;
 import com.codesurfers.xpto.repository.TransacaoFinanceiraRepository;
 
@@ -34,6 +39,40 @@ public class TransacaoFinanceiraController {
 		Page<TransacaoFinanceira> paginaTransacoes = transacaoFinanceiraRepository.findByValidoTrue(pageableRequest);
 
 		return paginaTransacoes.getContent();
+	}
+	
+	@RequestMapping(value = "/datatablesFormat")
+	public TransacoesFinanceirasDatatableDTO buscarValidosDatatablesFormat(
+			@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+			@RequestParam(value = "limit", required = false, defaultValue = "100") int limit) {
 
+		Pageable pageableRequest = PageRequest.of(page, limit);
+
+		Long quantidadeTransacoesValidas = transacaoFinanceiraRepository.countByValidoTrue();
+		
+		Page<TransacaoFinanceira> paginaTransacoes = transacaoFinanceiraRepository.findByValidoTrue(pageableRequest);
+		
+		TransacoesFinanceirasDatatableDTO transacaoDTO = new TransacoesFinanceirasDatatableDTO();
+		transacaoDTO.setRecordsTotal(quantidadeTransacoesValidas.intValue());
+		transacaoDTO.setRecordsFiltered(quantidadeTransacoesValidas.intValue());
+		
+		List<TransacaoFinanceira> transacoes = paginaTransacoes.getContent();
+		
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		
+		for (TransacaoFinanceira transacaoFinanceira : transacoes) {
+			List<String> valoresAtributos = new ArrayList<String>();
+			valoresAtributos.add(transacaoFinanceira.getId());
+			valoresAtributos.add(df.format(transacaoFinanceira.getDataHora().getTime()));
+			valoresAtributos.add(transacaoFinanceira.getDestinatario());
+			valoresAtributos.add(transacaoFinanceira.getValor().toString());
+			valoresAtributos.add(transacaoFinanceira.getTipoTransacao().toString());
+			valoresAtributos.add(transacaoFinanceira.isValido() ? "Sim" : "Não");
+			valoresAtributos.add(transacaoFinanceira.getErroValidacao().getDescricao());
+			
+			transacaoDTO.getData().add(valoresAtributos);
+		}
+
+		return transacaoDTO;
 	}
 }
