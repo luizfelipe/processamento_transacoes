0fc9d1bab079a076da41ae5a4713c6bcaa7f5639
joseneto86 <jose.neto86@gmail.com>
2018-09-23 15:04:54 -0300

    refatorando o controller

--- code_surfers-ws/src/main/java/com/codesurfers/xpto/controller/TransacaoFinanceiraController.java
+++ code_surfers-ws/src/main/java/com/codesurfers/xpto/controller/TransacaoFinanceiraController.java
@@ -16,8 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codesurfers.xpto.dto.TransacaoFinanceiraDadosDTO;
 import com.codesurfers.xpto.dto.TransacoesFinanceirasDatatableDTO;
 import com.codesurfers.xpto.model.TransacaoFinanceira;
+import com.codesurfers.xpto.model.enumerations.ErroValidacaoTransacao;
 import com.codesurfers.xpto.repository.TransacaoFinanceiraRepository;
 
 @RestController
@@ -91,6 +93,21 @@ public class TransacaoFinanceiraController {
 			result.add( Long.parseLong( tuple.get("qtd").toString() ));
 		}
 		return result;
+
+	}
+	
+	@RequestMapping(value = "/dados")
+	public TransacaoFinanceiraDadosDTO buscarDadosPainel(
+			@RequestParam(value = "ano", required = false, defaultValue = "2018") int ano) {
+		
+		Long semErro = this.transacaoFinanceiraRepository.countByErroValidacaoAndAnoArquivo(ErroValidacaoTransacao.SEM_ERRO, ano);
+	    Long regra1 = this.transacaoFinanceiraRepository.countByErroValidacaoAndAnoArquivo(ErroValidacaoTransacao.ERRO_001, ano);
+		Long regra2 = this.transacaoFinanceiraRepository.countByErroValidacaoAndAnoArquivo(ErroValidacaoTransacao.ERRO_002, ano);
+		Long regra3 = this.transacaoFinanceiraRepository.countByErroValidacaoAndAnoArquivo(ErroValidacaoTransacao.ERRO_003, ano);
+		Long regra4 = this.transacaoFinanceiraRepository.countByErroValidacaoAndAnoArquivo(ErroValidacaoTransacao.ERRO_004, ano);
+		
+		TransacaoFinanceiraDadosDTO dados = new TransacaoFinanceiraDadosDTO(semErro, regra1, regra2, regra3, regra4);
+		return dados;
 
 	}
 }
